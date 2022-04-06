44bb1065414090f457d279a5e9da96071bd77112
Nicole Steadman <nsteadman@gmail.com>
2022-04-05 18:14:07 -0700

    commit tree 25197a057ce9367148c73068c4dc0562c26bfcc1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
